71eeb9c38da99d51a72d6c4a240ee982dc4aac56
Alexander Gould <alexander.gould94@gmail.com>
2021-02-01 13:23:36 -0800

    commit tree fa3dffa33e21b92675c3502b966db89394719787

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,18 @@
 # Use this space to code along with the readme
+def say_hello_ten_times
+    phrase = "Hello World!"
+    name= "My name is Alexander"
+    puts phrase, name
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+say_hello_ten_times
+say_hello_ten_times
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
